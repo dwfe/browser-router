4c56be6f9edcc1c5add1798aafbc3bc72b3b1064
z <alex@dwfe.ru>
2020-11-17 16:50:09 +0300

    .

--- packages/tests-manual/src/routing/components/ActionData/ActionData.tsx
+++ packages/tests-manual/src/routing/components/ActionData/ActionData.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useLayoutEffect, useRef} from 'react'
+import autosize from 'autosize'
 import './styles.css'
 
-export const ActionData = ({actionData}: any) => ((<div className="ActionData">
-  <p className="ActionData_text">route action data (injected):</p>
-  <textarea className="ActionData_listing"
-            value={JSON.stringify(actionData || 'empty', null, 2)}
-            readOnly={true}/>
-</div>))
+export const ActionData = ({actionData}: any) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  useLayoutEffect(() => {
+    if (!!textareaRef.current)
+      autosize(textareaRef.current)
+  }, [textareaRef])
+
+  return (
+    <div className="ActionData">
+      <p className="ActionData_text">route action data (injected):</p>
+      <textarea className="ActionData_listing"
+                value={JSON.stringify(actionData || 'empty', null, 2)}
+                readOnly={true}
+                ref={textareaRef}/>
+    </div>
+  )
+}
