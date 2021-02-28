6aad57da47ceb2bf46774bd0c73c278678db33b4
z <alex@dwfe.ru>
2021-03-01 01:35:50 +0300

    .

--- packages/utils-node/src/automation-environment/index.ts
+++ packages/utils-node/src/automation-environment/index.ts
@@ -1,7 +1,4 @@
 export * from './abstract.task';
 export * from './automation.environment';
 export * from './contract';
-export * from './png.utils';
-export * from './req.interceptor';
 export * from './storage';
-export * from './task.executor';
